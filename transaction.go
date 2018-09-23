b2a22c19789a423135521300f2e5773ed6b41c25
akihito.nakano <sora.akatsuki@gmail.com>
2018-09-23 21:19:10 +0900

    Transaction has its hash value

--- transaction.go
+++ transaction.go
@@ -4,8 +4,9 @@ type Transaction struct {
 	Sender []byte
 	Recipient []byte
 	Amount int64
+	Hash []byte
 }
 
 func NewTransaction(sender []byte, recipient []byte, amount int64) *Transaction {
-	return &Transaction{sender, recipient, amount}
+	return &Transaction{sender, recipient, amount, []byte{}}
 }
