ccf5ec9031830fb15316bbcafa657eb257e0b49e
akihito.nakano <sora.akatsuki@gmail.com>
2018-09-29 23:12:13 +0900

    Add message struct

---
+++ message.go
@@ -0,0 +1,26 @@
+package main
+
+import "encoding/json"
+
+type Message struct {
+	Type MessageType
+	Data string
+}
+
+type MessageType int
+
+const (
+	MessageTypeLatestBlock MessageType = iota
+)
+
+func newLatestBlockMessage(latestBlock Block) (*Message, error) {
+	bytes, err := json.Marshal(latestBlock)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Message{
+		Type: MessageTypeLatestBlock,
+		Data: string(bytes),
+	}, nil
+}
